9717d321474bf212d5409e1d3ceb56d5f57c64e1
Shawn Ling <shawnling@gmail.com>
2015-11-13 08:13:05 +0800

    did assignment

--- R Programming/ProgrammingAssignment1/corr.R
+++ R Programming/ProgrammingAssignment1/corr.R
@@ -3,12 +3,12 @@ corr <- function(directory, threashold = 0) {
     comp <- complete(directory)
     comp_use <- subset(comp, nobs >= threashold)
     monitor_files <- 
-        dir(path = paste(getwd(), "/", directory, sep = ""))
+        dir(path = file.path(getwd(), directory))
     monitor_files = monitor_files[comp_use[,1]]
     no_files = length(monitor_files)
     if(no_files > 0)
         for (i in 1:no_files){
-            file_name <- paste(getwd(), "/", directory, "/", monitor_files[i], sep = "")
+            file_name <- file.path(getwd(), directory, monitor_files[i])
             pollution_data <- read.csv(file_name)
             data_sulfate = subset(pollution_data, select = sulfate)
             data_nitrate = subset(pollution_data, select = nitrate)
